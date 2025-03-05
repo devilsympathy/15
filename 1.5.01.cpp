dc651efcf21ac6f9e2311157ca207913d3c3dc9a
devilsympathy <96268601+devilsympathy@users.noreply.github.com>
2025-03-05 08:02:18 +0300

    Update 1.5.01.cpp

--- 1.5.01.cpp
+++ 1.5.01.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 
-int main()
+int main() 
 {
-	setlocale(LC_ALL, "ru");
-	int arr[10]{ 5,3,5,6,2,3,2,5,6,11 }, minimalValue{ arr[0] }, maximumValue{ arr[0] };
-	std::cout << "Массив: ";
-	for (int i = 0; i <= 9; i++)
-	{
-		std::cout << arr[i] << " ";
-		minimalValue = (minimalValue >= arr[i] ? arr[i] : minimalValue);
-		maximumValue = (maximumValue <= arr[i] ? arr[i] : maximumValue);
-	}
-	std::cout << std::endl << "Минимальный элемент: " << minimalValue << std::endl;
-	std::cout << "Максимальный элемент: " << maximumValue;
-	return 0;
+    int arr[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int i = 0; i < 9; i++) 
+    {
+        std::cout << arr[i] << ", ";
+    }
+    std::cout << arr[9] << std::endl;
+    return 0;
 }
